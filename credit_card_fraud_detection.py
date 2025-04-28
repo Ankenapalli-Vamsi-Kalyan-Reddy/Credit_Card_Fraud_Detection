d78d49081c301c6f960a67e5511ccd8094bbd3ba
Ankenapalli-Vamsi-Kalyan-Reddy <avamsikalyanreddy@gmail.com>
2025-04-27 21:54:56 -0400

    Implemented Logistic Regression model with accuracy evaluation on training and test sets

--- credit_card_fraud_detection.py
+++ credit_card_fraud_detection.py
@@ -43,3 +43,25 @@ new_dataset.groupby('Class').mean()
 
 X = new_dataset.drop(columns='Class', axis=1)
 Y = new_dataset['Class']
+
+print(X)
+
+print(Y)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y, random_state=2)
+
+print(X.shape, X_train.shape, X_test.shape)
+
+model = LogisticRegression()
+
+model.fit(X_train, Y_train)
+
+X_train_prediction = model.predict(X_train)
+training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
+
+print(training_data_accuracy)
+
+X_test_prediction = model.predict(X_test)
+test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
+
+print(test_data_accuracy)
